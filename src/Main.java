7e4baad9d00d9370d1ea64706ef3afa21bf2a31a
Petoovee <Petomatick@gmail.com>
2018-09-07 07:20:15 +0200

    Stable version

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,37 @@ public class Main
 	public static void main(String[] args){
 		try{
 		LinkedList<String> fileList = new LinkedList<String>();
-		File file = new File("Auctiontools-Core.lua");
+		File file = new File(System.getProperty("user.dir") +"\\Auctiontools-Core.lua");
+		System.out.println(file.toString());
 		BufferedReader reader = new BufferedReader(new FileReader(file));
-		FileWriter writer = new FileWriter("Auctiontools-Core.lua");
 		Boolean lineFound = false;
+		Boolean patched = false;
 		String line = reader.readLine();
 		
 		if (line == null) System.out.println("File not found!");
 			
 		
 		while (line != null) {
+			fileList.addLast(line);
 			if(line.equals("		elseif stack_count*stack_size>Appraiser.GetTrueItemCount(Appraiser.ActiveSellingItem.itemid,Appraiser.ActiveSellingItem.link) then")){
-				System.out.println("Line found!");
+				System.out.println("Line found at row " +fileList.size() +"!");
 				lineFound = true;
 			}
-			if(!line.equals("			Appraiser:SetMaxStackSize") && lineFound){
-				fileList.addLast("			Appraiser:SetMaxStackSize");
+			if(!line.equals("			Appraiser:SetMaxStackSize()") && lineFound && !patched){
+				fileList.addLast("			Appraiser:SetMaxStackSize()");
+				patched = true;
 			}
 			line = reader.readLine();
-			fileList.addLast(line);
-			System.out.println("DEBUG: " +line);
 		}
 		
 		reader.close();
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
 		
 		for (int i = 0; i < fileList.size(); i++){
-			writer.append(fileList.get(i));
+			writer.write(fileList.get(i));
+			writer.newLine();
 		}
+		
 		writer.close();
 		System.out.println("Done patching Auctiontools-Core.lua");
 		
